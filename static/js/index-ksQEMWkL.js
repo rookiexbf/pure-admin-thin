324d38e4b088e547ac84d19c4d672042ce3427f7
rookiexbf <rookiexbf@users.noreply.github.com>
2025-03-27 09:35:54 +0000

    deploy: c56da90b1d73edf2931a1fa8f07dd88fbddcb6d1

---
+++ static/js/index-ksQEMWkL.js
@@ -0,0 +1,1 @@
+import{d as e,c as n,o}from"./index-CI2g6m2Y.js";const m=e({name:"Welcome",__name:"index",setup(r){return(t,a)=>(o(),n("h1",null,"Pure-Admin-Thin（rookiex）"))}});export{m as default};
